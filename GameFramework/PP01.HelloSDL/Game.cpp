a8e0830d42d49752ac17458522bdfd46efa35dc2
yeojeong <user@532-20>
2018-11-13 13:00:20 +0900

    PP05.AnimatingSprite

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -1,39 +1,61 @@
-#include <SDL.h>
 #include "Game.h"
 #include <iostream>
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
 
 bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos,
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos,
 			width, height, fullscreen);
-		if (g_pWindow != 0)
+
+
+		if (m_pWindow != 0)
 		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
+		m_bRunning = true;
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
+
+
+
 	}
 	else {
 		return false;
 	}
+
 	return true;
 }
 
 void Game::render()
 {
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::update()
+{
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Game::clean()
 {
 	std::cout << "cleaning game\n";
-	SDL_DestroyWindow(g_pWindow);
-	SDL_DestroyRenderer(g_pRenderer);
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
 	SDL_Quit();
 }
 
@@ -45,7 +67,7 @@ void Game::handleEvent()
 		switch (event.type)
 		{
 		case SDL_QUIT:
-			g_bRunning = false;
+			m_bRunning = false;
 			break;
 		default:
 			break;
